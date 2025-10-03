0602c0cc285b393a5bf6f265561db26477128607
stephane-nawrat <stephane.nawrat@laplateforme.io>
2025-10-03 16:57:10 +0200

    feat(header): add logo and nav, mobile-first responsive

--- partials/header.php
+++ partials/header.php
@@ -1,5 +1,19 @@
 <header>
-  <h1>AppFavorites 2025</h1>
+  <!-- Logo -->
+  <div class="logo">
+    <h1>AppFavorites</h1>
+  </div>
+
+  <!-- Navigation -->
+  <nav>
+    <ul>
+      <li><a href="index.php">Accueil</a></li>
+      <li><a href="app1.php">App1</a></li>
+      <li><a href="app2.php">App2</a></li>
+      <li><a href="app3.php">App3</a></li>
+      <li><a href="contact.php">Contact</a></li>
+    </ul>
+  </nav>
 </header>
 
-<!-- barre de navigation, logo, menu -->
+
